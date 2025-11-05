ecb2b6627617a3d8621138c0bd6974634d37b5b9
elenac28 <emvaccarino@gmail.com>
2025-11-05 16:39:44 -0500

    feat: bump to version 1.0.4

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (_req, res) => {
-    res.json({ status: "ok", service: "hello-cicd", version: "1.0.3" });
+    res.json({ status: "ok", service: "hello-cicd", version: "1.0.4" });
 });
 
 const port = process.env.PORT || 8080;
